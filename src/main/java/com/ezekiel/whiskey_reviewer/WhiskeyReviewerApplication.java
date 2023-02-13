389d15045c7ee4419f7d55f1e4f1c162cd3d0cc6
Ezekiel <umanmah234@gmail.com>
2023-02-13 16:34:09 -0500

    initial commit

---
+++ src/main/java/com/ezekiel/whiskey_reviewer/WhiskeyReviewerApplication.java
@@ -0,0 +1,13 @@
+package com.ezekiel.whiskey_reviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiskeyReviewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiskeyReviewerApplication.class, args);
+	}
+
+}
