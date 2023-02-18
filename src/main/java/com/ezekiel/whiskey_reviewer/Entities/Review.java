77d923615b0f243a73f0998c285238af4cbdbfea
Ezekiel <umanmah234@gmail.com>
2023-02-18 18:25:04 -0500

    remove setters/getters

--- src/main/java/com/ezekiel/whiskey_reviewer/Entities/Review.java
+++ src/main/java/com/ezekiel/whiskey_reviewer/Entities/Review.java
@@ -39,28 +39,4 @@ public class Review {
             this.rating = reviewDTO.getRating();
         }
     }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getComments() {
-        return comments;
-    }
-
-    public void setComments(String comments) {
-        this.comments = comments;
-    }
-
-    public int getRating() {
-        return rating;
-    }
-
-    public void setRating(int rating) {
-        this.rating = rating;
-    }
 }
