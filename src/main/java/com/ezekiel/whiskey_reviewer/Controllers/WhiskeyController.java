314153e8b622c876135e5152a41e031814d2d70f
Ezekiel <umanmah234@gmail.com>
2023-02-18 23:37:29 -0500

    add whiskey controller

---
+++ src/main/java/com/ezekiel/whiskey_reviewer/Controllers/WhiskeyController.java
@@ -0,0 +1,22 @@
+package com.ezekiel.whiskey_reviewer.Controllers;
+
+import com.ezekiel.whiskey_reviewer.DTOs.WhiskeyDTO;
+import com.ezekiel.whiskey_reviewer.Services.WhiskeyServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/whiskey")
+public class WhiskeyController {
+    @Autowired
+    private WhiskeyServiceImpl whiskeyService;
+    @PostMapping("/add")
+    public List<String> addWhiskey(@RequestBody WhiskeyDTO whiskeyDTO){
+        return whiskeyService.addWhiskey(whiskeyDTO);
+    }
+}
