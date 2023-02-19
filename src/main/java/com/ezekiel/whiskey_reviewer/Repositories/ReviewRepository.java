ae07c563eb083fe3afdc2c79e12f9ded36cf3d83
Ezekiel <umanmah234@gmail.com>
2023-02-18 21:34:36 -0500

    add custom findAllBy methods

--- src/main/java/com/ezekiel/whiskey_reviewer/Repositories/ReviewRepository.java
+++ src/main/java/com/ezekiel/whiskey_reviewer/Repositories/ReviewRepository.java
@@ -3,5 +3,10 @@ package com.ezekiel.whiskey_reviewer.Repositories;
 import com.ezekiel.whiskey_reviewer.Entities.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    List<Review> findAllByUserId(Long userId);
+
+    List<Review> findAllByWhiskeyId(Long whiskeyId);
 }
