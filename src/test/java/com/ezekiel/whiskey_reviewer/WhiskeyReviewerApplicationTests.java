d1509a54c49398cf1012d56ff1261a1e3d1bfae6
Ezekiel <umanmah234@gmail.com>
2023-02-19 21:11:10 -0500

    initial commit

---
+++ src/test/java/com/ezekiel/whiskey_reviewer/WhiskeyReviewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezekiel.whiskey_reviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiskeyReviewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
