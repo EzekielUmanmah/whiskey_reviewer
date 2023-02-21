117f29b7897c256fa648b43fb36eb1dca5a1ab46
Ezekiel <umanmah234@gmail.com>
2023-02-21 08:33:24 -0500

    enable cors (allow client reqs to server)

--- src/main/java/com/ezekiel/whiskey_reviewer/WhiskeyReviewerApplication.java
+++ src/main/java/com/ezekiel/whiskey_reviewer/WhiskeyReviewerApplication.java
@@ -2,12 +2,25 @@ package com.ezekiel.whiskey_reviewer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class WhiskeyReviewerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WhiskeyReviewerApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
 	}
 
 }
