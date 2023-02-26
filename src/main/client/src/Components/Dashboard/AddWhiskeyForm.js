6c54ae34a021f5ca24b61eb51355257f08e5651a
Ezekiel <umanmah234@gmail.com>
2023-02-26 01:55:35 -0500

    modify Button import

--- src/main/client/src/Components/Dashboard/AddWhiskeyForm.js
+++ src/main/client/src/Components/Dashboard/AddWhiskeyForm.js
@@ -3,7 +3,7 @@ import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
 import TextareaAutosize from '@mui/base/TextareaAutosize';
-import { Button } from '@mui/material';
+import Button from '@mui/material/Button';
 import { addWhiskey, getAllWhiskies } from '../../api/api';
 
 const initialState = {
