f336746decf0bb61b3b0d58eb3840bf6c3096b68
Ezekiel <umanmah234@gmail.com>
2023-02-25 22:04:00 -0500

    add routings

--- src/main/client/src/App.js
+++ src/main/client/src/App.js
@@ -1,7 +1,18 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Login from './Components/LandingPage/Login';
+import SignUp from './Components/LandingPage/SignUp';
+import Dashboard from './Components/Dashboard/Dashboard';
+
 function App() {
   return (
-    <div className='App'>
-      <h1>Hello Whiskey!</h1>
+    <div>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+      </Routes>
     </div>
   );
 }
