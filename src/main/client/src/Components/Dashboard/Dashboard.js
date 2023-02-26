d873c763fc08f8faeb01aba64c58528c345fb07a
Ezekiel <umanmah234@gmail.com>
2023-02-25 22:12:10 -0500

    add Dashboard

--- src/main/client/src/Components/Dashboard/Dashboard.js
+++ src/main/client/src/Components/Dashboard/Dashboard.js
@@ -47,17 +47,6 @@ export default function Dashboard() {
         <Whiskies newWhiskey={newWhiskey} setIsReview={setNewWhiskey} />
       )}
       <Box sx={{ bgcolor: 'background.paper', p: 6 }} component='footer'>
-        <Typography variant='h6' align='center' gutterBottom>
-          Footer
-        </Typography>
-        <Typography
-          variant='subtitle1'
-          align='center'
-          color='text.secondary'
-          component='p'
-        >
-          Something here to give the footer a purpose!
-        </Typography>
         <Copyright />
       </Box>
     </ThemeProvider>
