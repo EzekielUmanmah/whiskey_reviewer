af2a90915650ba461d7bc90dafc0beee01cbeae1
Ezekiel <umanmah234@gmail.com>
2023-02-26 23:21:28 -0500

    add unique user email check in addUser

--- src/main/java/com/ezekiel/whiskey_reviewer/Services/UserServiceImpl.java
+++ src/main/java/com/ezekiel/whiskey_reviewer/Services/UserServiceImpl.java
@@ -22,24 +22,29 @@ public class UserServiceImpl implements UserService {
     @Transactional
     public List<String> addUser(UserDTO userDTO){
         List<String> response = new ArrayList<>();
+        Optional<User> userOptional = userRepository.findByEmail(userDTO.getEmail());
 
-        User user = new User(userDTO);
-        userRepository.saveAndFlush(user);
-
-        response.add("New user added!");
+        if(userOptional.isEmpty()){
+            User user = new User(userDTO);
+            userRepository.saveAndFlush(user);
+            response.add("New user added!");
+        } else {
+            response.add("An account with this email already exists!");
+        }
         return response;
     }
     @Override
     @Transactional
     public List<String> userLogin(UserDTO userDTO){
         List<String> response = new ArrayList<>();
-        Optional<User> userOptional = userRepository.findById(userDTO.getId());
+        Optional<User> userOptional = userRepository.findByEmail(userDTO.getEmail());
 
         if(userOptional.isPresent() && passwordEncoder.matches(userDTO.getPassword(), userOptional.get().getPassword())){
-            response.add("http://localhost:4000/home.html");
+//            response.add("http://localhost:4000/home.html");
             response.add(String.valueOf(userOptional.get().getId()));
+//            response.add(String.valueOf(userDTO));
         } else {
-            response.add("Invalid username or password.");
+            response.add("Invalid email or password.");
         }
 
         return response;
