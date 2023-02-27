4be2b2f3de45e55f6e02df1404e81eb79dea6821
Ezekiel <umanmah234@gmail.com>
2023-02-26 23:15:05 -0500

    add getAllWhiskies method

--- src/main/java/com/ezekiel/whiskey_reviewer/Controllers/WhiskeyController.java
+++ src/main/java/com/ezekiel/whiskey_reviewer/Controllers/WhiskeyController.java
@@ -1,12 +1,10 @@
 package com.ezekiel.whiskey_reviewer.Controllers;
 
 import com.ezekiel.whiskey_reviewer.DTOs.WhiskeyDTO;
+import com.ezekiel.whiskey_reviewer.Entities.Whiskey;
 import com.ezekiel.whiskey_reviewer.Services.WhiskeyServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,6 +13,8 @@ import java.util.List;
 public class WhiskeyController {
     @Autowired
     private WhiskeyServiceImpl whiskeyService;
+    @GetMapping
+    public List<Whiskey> getAllWhiskies(){ return whiskeyService.getAllWhiskies(); }
     @PostMapping
     public List<String> addWhiskey(@RequestBody WhiskeyDTO whiskeyDTO){
         return whiskeyService.addWhiskey(whiskeyDTO);
