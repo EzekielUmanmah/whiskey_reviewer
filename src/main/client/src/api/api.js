1516a67cf31c5ad55ba96606b97740a306862465
Ezekiel <umanmah234@gmail.com>
2023-02-27 02:59:53 -0500

    add update route

--- src/main/client/src/api/api.js
+++ src/main/client/src/api/api.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
+axios.defaults.headers.delete['Content-Type'] = 'application/json';
+axios.defaults.headers.delete['Access-Control-Allow-Origin'] = '*';
+
 const baseURL = 'http://localhost:4000';
 
 export const login = (data) => axios.post(`${baseURL}/api/users/login`, data);
@@ -10,3 +13,9 @@ export const getAllWhiskies = () => axios.get(`${baseURL}/api/whiskey`);
 export const addWhiskey = (data) => axios.post(`${baseURL}/api/whiskey`, data);
 
 export const addReview = (data) => axios.post(`${baseURL}/api/review`, data);
+export const getAllReviewsByUser = (userId) =>
+  axios.get(`${baseURL}/api/review/user?userId=${userId}`);
+export const deleteReviewByReviewId = (reviewId) =>
+  axios.delete(`${baseURL}/api/review/${reviewId}`);
+export const updateReview = (updateReview) =>
+  axios.put(`${baseURL}/api/review`, updateReview);
