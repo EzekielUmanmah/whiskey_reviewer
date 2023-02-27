e625da3845a89cca07b22323f49470ad1634a236
Ezekiel <umanmah234@gmail.com>
2023-02-26 23:17:37 -0500

    add findAllByUser method signature

--- src/main/java/com/ezekiel/whiskey_reviewer/Repositories/ReviewRepository.java
+++ src/main/java/com/ezekiel/whiskey_reviewer/Repositories/ReviewRepository.java
@@ -1,6 +1,8 @@
 package com.ezekiel.whiskey_reviewer.Repositories;
 
+import com.ezekiel.whiskey_reviewer.DTOs.ReviewDTO;
 import com.ezekiel.whiskey_reviewer.Entities.Review;
+import com.ezekiel.whiskey_reviewer.Entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,4 +11,6 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
     List<Review> findAllByUserId(Long userId);
 
     List<Review> findAllByWhiskeyId(Long whiskeyId);
+
+    List<Review> findAllByUser(User user);
 }
